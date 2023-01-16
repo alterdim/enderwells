51393c461a59029ecd41212ccd98bb9eb4365ef1
lgerard <louis.gerard@epfl.ch>
2023-01-16 17:50:41 +0100

    refactoring

---
+++ src/main/java/com/alternis/sculkwells/SculkWells.java
@@ -0,0 +1,62 @@
+package com.alternis.sculkwells;
+
+import com.alternis.sculkwells.items.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SculkWells.MOD_ID)
+public class SculkWells
+{
+    public static final String MOD_ID = "sculkwells";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SculkWells()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
+           modEventBus.addListener(this::commonSetup);
+        modEventBus.register(this);
+
+
+
+    }
+
+    @SubscribeEvent
+    public void buildContents(CreativeModeTabEvent.BuildContents event) {
+        // Add to ingredients tab
+        if (event.getTab() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.DARK_DRILL);
+        }
+
+
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+
+
+
+    }
+}
